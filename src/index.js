8717c94cfaf8b6bc52db6735da64921cc6a3da59
Dongjoo Kim <50216530+mosmomedia@users.noreply.github.com>
2022-09-15 05:06:02 +0700

    fix: changed css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/fontStyles.css';
+// import './styles/fontStyles.css';
 import GlobalStyles from './styles/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
