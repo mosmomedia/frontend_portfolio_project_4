80fada679d09a7c2453a6a60c9c27d4ac6178f53
Dongjoo Kim <50216530+mosmomedia@users.noreply.github.com>
2022-09-26 03:17:23 +0700

    feat: added component // scroll to top

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NotFound from './components/shared/NotFound';
 
+import NotFound from './components/shared/NotFound';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import ProjectStorytunes from './pages/projects/Project-storytunes';
 
 function App() {
 	return (
 		<Router>
-			<Routes>
-				<Route path="/*" element={<NotFound />} />
-				<Route path="/" element={<Home />} />
-				{/* Portfolio - project links */}
-				<Route path="/project/storytunes" element={<ProjectStorytunes />} />
-			</Routes>
+			<ScrollToTop>
+				<Routes>
+					<Route path="/*" element={<NotFound />} />
+					<Route path="/" element={<Home />} />
+
+					{/* Portfolio - projects pages */}
+					<Route path="/project/storytunes" element={<ProjectStorytunes />} />
+				</Routes>
+			</ScrollToTop>
 		</Router>
 	);
 }
